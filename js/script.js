08a48f2183f625b5a57b6781e3c95b96d96f04c0
Marta <marta.tatchyn@gmail.com>
2025-06-02 20:11:52 +0100

    HW29

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 const btncOpenModal = document.querySelectorAll("[data-open-modal]")
 const bacdrop = document.querySelector(".bacdrop")
 const btncCloseModal = document.querySelector(".modal-close")
+const modal = document.querySelector(".modal")
 
 btncOpenModal.forEach((btn) => {
    btn.addEventListener("click", () => {
     bacdrop.classList.remove("is-hidden")
     document.body.style.overflow = "hidden"
+    modal.classList.add("rotate") 
    })
 })
 
 btncCloseModal.addEventListener("click", () => {
     bacdrop.classList.add("is-hidden")
         document.body.style.overflow = "auto"
+        modal.classList.remove("rotate") 
    })
