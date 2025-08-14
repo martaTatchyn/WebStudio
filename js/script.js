b01f1d20ccaf9f326af46b3f539e265920068be7
Marta <marta.tatchyn@gmail.com>
2025-08-14 14:14:11 +0100

    burger

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ const btncOpenModal = document.querySelectorAll("[data-open-modal]")
 const bacdrop = document.querySelector(".bacdrop")
 const btncCloseModal = document.querySelector(".modal__close")
 const modal = document.querySelector(".modal")
+const burger = document.querySelector('.header__burger')
+const mobileMenu = document.querySelector('.header__menu')
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle("burger-active");
+  mobileMenu.classList.toggle("menu-active");
+});
 
 btncOpenModal.forEach((btn) => {
    btn.addEventListener("click", () => {
